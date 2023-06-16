f37c348f74ebef84b1535f4f4dd920a9a9e03f3a
Beekay16 <kuyeboaz2020@gmail.com>
2023-06-15 22:27:59 -0700

    commit for 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-
-# < stdio.h >
+#include <stdio.h>
 /**
- * main . A program with puts function
- * Return:0(valid)
+ * main . Puts function program
+ * Return: 0 (valid)
  */
 int main(void)
 {
-puts("\"Programming is like a multilingual puzzle");
-return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
