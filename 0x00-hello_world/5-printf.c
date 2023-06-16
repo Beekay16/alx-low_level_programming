cd3ed86e26d8cc863cb4f8657e49f570ff2a9e35
Beekay16 <kuyeboaz2020@gmail.com>
2023-06-15 23:10:14 -0700

    C programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - printf function program using c language.
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is always a piece of art,\n");
+return (0);
+}
